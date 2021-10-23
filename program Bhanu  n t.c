a86b6bd531c909ac85608bf23b68b807c9149c87
Bhanu prakash <92910119+20P35A0372@users.noreply.github.com>
2021-10-23 12:31:17 +0530

    Add files via upload

---
+++ program Bhanu  n t.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("student\tage\tcgpa\n");
+	printf("satya\t19\t9.0\n");
+	printf("bhanu\t20\t8.7\n");
+	printf("navin\t20\t8.8\ngovindh\t20\t9.5\nsai\t19\t8.0\n");
+}
