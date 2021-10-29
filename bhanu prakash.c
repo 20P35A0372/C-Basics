d0e09e52065c7af48316b14ea3dd021b9b367f59
Bhanu prakash <92910119+20P35A0372@users.noreply.github.com>
2021-10-29 00:12:02 -0700

    Add files via upload

---
+++ bhanu prakash.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	 //Variable declaration 
+	int age;
+	float cgpa;
+	char initial;
+	
+	//Initialization
+	 age=20;
+   	cgpa=8.7;
+	initial='k';
+	
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c\n",initial);
+ } 
+
